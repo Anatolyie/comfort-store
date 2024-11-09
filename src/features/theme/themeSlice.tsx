79043eee37458ee225aa9c8f70b48c3a50490d21
Anatolyie <110687760+Anatolyie@users.noreply.github.com>
2024-11-09 20:27:33 +0100

    project initialization

---
+++ src/features/theme/themeSlice.tsx
@@ -0,0 +1,13 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+const initialState = {
+  name: "theme slice",
+};
+
+const themeSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {},
+});
+
+export default themeSlice.reducer;
