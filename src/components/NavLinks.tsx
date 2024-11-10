704ad953a9e2a62d89f0d1a359c41753e5c1ddcd
Anatolyie <110687760+Anatolyie@users.noreply.github.com>
2024-11-10 11:50:46 +0100

    change theme

--- src/components/NavLinks.tsx
+++ src/components/NavLinks.tsx
@@ -1,7 +1,24 @@
-import React from "react";
-
+import { links } from "@/utils";
+import { NavLink } from "react-router-dom";
 function NavLinks() {
-  return <div>NavLinks</div>;
+  return (
+    <div className="hidden lg:flex justify-center items-center gap-x-4">
+      {links.map((link) => {
+        return (
+          <NavLink
+            to={link.href}
+            key={link.label}
+            className={({ isActive }) => {
+              return `capitalize font-light tracking-wide ${
+                isActive ? "text-primary" : ""
+              }`;
+            }}
+          >
+            {link.label}
+          </NavLink>
+        );
+      })}
+    </div>
+  );
 }
-
 export default NavLinks;
